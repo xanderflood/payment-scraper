c3d6e845907ed1640899843fb5834ddea74b1eaa
Xander Flood <xander.t.flood@gmail.com>
2020-12-30 13:47:21 -0500

    fix docker script

---
+++ build_and_push.sh
@@ -0,0 +1,10 @@
+/bin/sh -e
+
+export PREFIX=`if [ "$TRAVIS_BRANCH" == "master" ]; then echo ""; else echo "staging-" ; fi`
+export docker_repo=xanderflood/payment
+export docker_build_directory=.
+
+docker login -u $DOCKER_USER -p $DOCKER_PASS
+name=main ./build_and_push_image.sh
+name=web ./build_and_push_image.sh
+name=dbmgr ./build_and_push_image.sh
---
+++ build_and_push_image.sh
@@ -0,0 +1,8 @@
+docker build $docker_build_directory -t $docker_repo:${PREFIX}${name} -f dockerfiles/${name}.Dockerfile
+
+tags="${PREFIX}${name}-build-${TRAVIS_BUILD_NUMBER},${PREFIX}${name}-commit-${TRAVIS_COMMIT::8},${PREFIX}${name}"
+for tag in ${tags//,/ }
+do
+  docker tag $docker_repo:${PREFIX}${name} $docker_repo:$tag
+  docker push $docker_repo:$tag
+done
