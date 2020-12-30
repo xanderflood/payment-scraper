5088e2aa0e21538bf625fbcb11ba83e438981f2d
Xander Flood <xander.t.flood@gmail.com>
2020-12-30 13:56:42 -0500

    more verbosity

--- build_and_push_image.sh
+++ build_and_push_image.sh
@@ -1,3 +1,5 @@
+/bin/sh -xe
+
 docker build $docker_build_directory -t $docker_repo:${PREFIX}${name} -f dockerfiles/${name}.Dockerfile
 
 tags="${PREFIX}${name}-build-${TRAVIS_BUILD_NUMBER},${PREFIX}${name}-commit-${TRAVIS_COMMIT::8},${PREFIX}${name}"
