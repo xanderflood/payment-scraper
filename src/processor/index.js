992caffeed10ff4fd15b630da3fe23c0ebfd5b4b
Xander Flood <xander.t.flood@gmail.com>
2020-12-30 21:19:43 -0500

    fix case insensitivity

--- src/processor/index.js
+++ src/processor/index.js
@@ -1,10 +1,11 @@
 const Logger = require('node-json-logger');
+const { errString } = require('../utils')
 const logger = new Logger();
 
 const numericEqualityThreshold = 0.01;
 
 function iMatch(string, regex) {
-	return !!((string || "").match(new RegExp(regex), "i"))
+	return !!((string || "").match(new RegExp(regex, "i")))
 }
 function roughlyEquals(val, ref) {
 	return Math.abs(val - ref) < numericEqualityThreshold;
@@ -13,7 +14,7 @@ function roughlyEquals(val, ref) {
 function buildMatcher(rule) {
 	switch (rule.type) {
 	case "regex":
-		return (transaction) => iMatch(transaction[rule.field], rule.string);
+		return (transaction) => iMatch(transaction[rule.field], rule.string)
 	case "numeric":
 		return (transaction) => roughlyEquals(rule.number, transaction[rule.field]);
 	case "csv_field_regex":
@@ -49,31 +50,32 @@ class Processor {
 	async initialize() {
 		try {
 			this.categorizers = (await this.database.getRules()).map(compile);
-		} catch (e) {
-			logger.error(`failed fetching categorization rules - rethrowing`, e);
+		} catch (error) {
+			logger.error(`failed fetching categorization rules - rethrowing`, errString(error));
 			throw e;
 		}
 	}
 	async processTransactions() {
-    try {
-      var trs = await this.database.getUnprocessedTransactions();
-    } catch (e) {
-      logger.error(`failed fetching unprocessed transaction - rethrowing`, e);
-      throw e;
-    }
+		try {
+			var trs = await this.database.getUnprocessedTransactions();
+		} catch (error) {
+			logger.error(`failed fetching unprocessed transaction - rethrowing`, errString(error));
+			throw e;
+		}
 
-    for (var i = trs.length - 1; i >= 0; i--) {
-      try {
-        let update = await this.processTransaction(trs[i]);
+		for (var i = trs.length - 1; i >= 0; i--) {
+			try {
+				let update = await this.processTransaction(trs[i]);
 
-        if (update) {
-          await this.database.saveTransactionProcessingResult(trs[i].id, update);
-        }
-      } catch (e) {
-        logger.error(`failed processing transaction - rethrowing`, e);
-        throw e;
-      }
-    }
+				if (update) {
+					await this.database.saveTransactionProcessingResult(trs[i].id, update);
+					break;
+				}
+			} catch (error) {
+				// TODO include skipped errors in API response
+				logger.error(`failed processing transaction - carrying on`, errString(error));
+			}
+		}
 	}
 	async processTransaction(transaction) {
 		for (var i = this.categorizers.length - 1; i >= 0; i--) {
@@ -81,7 +83,7 @@ class Processor {
 				const update = this.categorizers[i](transaction);
 				if (update) return update;
 			} catch (error) {
-				logger.error("error occurred while evaluating rule - carrying on", e)
+				logger.error("error occurred while evaluating rule - carrying on", errString(error))
 			}
 		}
 	}
