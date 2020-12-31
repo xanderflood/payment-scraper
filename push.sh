d3b0aa2e6f2eeb3a809f78af84e1512aeed35ecd
Xander Flood <xander.t.flood@gmail.com>
2020-12-30 19:56:03 -0500

    fix repo ref

--- push.sh
+++ push.sh
@@ -3,7 +3,7 @@ set -xe
 tags="${PREFIX}${name}-build-${TRAVIS_BUILD_NUMBER},${PREFIX}${name}-commit-${TRAVIS_COMMIT::8},${PREFIX}${name}"
 for tag in ${tags//,/ }
 do
-  echo "Pushing local:${name} as $docker_repo:$tag"
-  docker tag local:${name} $docker_repo:$tag
-  docker push $docker_repo:$tag
+  echo "Pushing local:${name} as xanderflood/payment:$tag"
+  docker tag local:${name} xanderflood/payment:$tag
+  docker push xanderflood/payment:$tag
 done
