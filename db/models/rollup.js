56140c2f7f2ac01f195c63a13d778ce745596487
Xander Flood <xander.t.flood@gmail.com>
2021-01-08 19:38:34 -0500

    fix model code

--- db/models/rollup.js
+++ db/models/rollup.js
@@ -8,7 +8,7 @@ module.exports = (sequelize, DataTypes) => {
     // NOTE: autoIncrement is a hack to prevent Sequelize from pushing a null value on create
     id: { type: DataTypes.UUID, primaryKey: true, autoIncrement: true },
     monthStart: { type: DataTypes.DATE, field: "month_start", allowNull: false, unique: true },
-    rollup:     { type: Sequelize.JSONB, allowNull: false },
+    rollup:     { type: DataTypes.JSONB, allowNull: false },
   }, {
     sequelize,
     modelName: 'Rollup',
