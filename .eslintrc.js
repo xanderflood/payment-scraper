caf2b430c790f571615a211d59d5e6b29404b458
Xander Flood <xander.t.flood@gmail.com>
2021-01-19 20:09:20 -0500

    get it started!

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es6: true,
+  },
+  extends: ['prettier', 'airbnb-base'],
+  plugins: ['prettier'],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+  },
+  rules: {
+    'prettier/prettier': 'error',
+    quotes: 'off',
+  },
+};
